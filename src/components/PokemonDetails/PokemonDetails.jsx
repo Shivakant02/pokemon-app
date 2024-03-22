521fcbc244fa34247a952a4cea22d02ec25a8be5
Shivakant <kants6397@gmail.com>
2024-03-22 12:58:34 +0530

    Refactor PokemonDetails component and add usePokemon hook

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,37 +1,11 @@
-// import React from 'react'
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react'
+
 import './PokemonDetails.css'
 import { useParams } from 'react-router-dom'
-import axios from 'axios';
-import { useState } from 'react';
+import usePokemon from '../../hooks/usePokemon';
 function PokemonDetails() {
     const { id } = useParams();
-    const POKE_DETAIL_URL = 'https://pokeapi.co/api/v2/pokemon/';
-
-    const [pokemon, setPokemon] = useState(null);
-
-
-   async function downloadPokemon() {
-       const response = await axios.get(POKE_DETAIL_URL + id)
-       const pokemon = response.data;
-       //    console.log(pokemon);
-       setPokemon(
-           {
-               name: pokemon.name,
-               height: pokemon.height,
-               weight: pokemon.weight,
-               types: pokemon.types,
-               image:pokemon.sprites.other.dream_world.front_default
-           }
-       )
-
-    }
-
-
-    useEffect(() => {
-        downloadPokemon()
-    },[])
+    const [pokemon] = usePokemon(id);
     return (
         <>
             <h1>
